e34a6c72835b8d551620408ef380730c2cff581e
thai <10421054@student.vgu.edu.vn>
2024-06-05 23:59:15 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,19 @@ const path = require('path');
 
 const router = express.Router();
 const metricsFilePath = path.join(__dirname, '../metrics.json');
+const archiveFilePath = path.join(__dirname, '../archive.json');
+
+// Move current metrics to archive on server start
+if (fs.existsSync(metricsFilePath)) {
+  const data = fs.readFileSync(metricsFilePath);
+  let archivedMetrics = [];
+  if (fs.existsSync(archiveFilePath)) {
+    archivedMetrics = JSON.parse(fs.readFileSync(archiveFilePath));
+  }
+  archivedMetrics.push(...JSON.parse(data));
+  fs.writeFileSync(archiveFilePath, JSON.stringify(archivedMetrics, null, 2));
+  fs.unlinkSync(metricsFilePath);
+}
 
 // Endpoint to receive metrics from clients
 router.post('/metrics', (req, res) => {
@@ -25,10 +38,20 @@ router.post('/metrics', (req, res) => {
   res.send({ status: 'success' });
 });
 
-// Endpoint to fetch all metrics
+// Endpoint to fetch all current metrics
 router.get('/all-metrics', (req, res) => {
   if (fs.existsSync(metricsFilePath)) {
     const data = fs.readFileSync(metricsFilePath);
+    res.send(JSON.parse(data));
+  } else {
+    res.send([]);
+  }
+});
+
+// Endpoint to fetch archived metrics
+router.get('/archived-metrics', (req, res) => {
+  if (fs.existsSync(archiveFilePath)) {
+    const data = fs.readFileSync(archiveFilePath);
     res.send(JSON.parse(data));
   } else {
     res.send([]);
