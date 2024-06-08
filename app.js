789536daf950dd44cb2c9a45b8b99435e1b93bf5
thai <10421054@student.vgu.edu.vn>
2024-06-08 16:13:55 +0700

    test network

--- app.js
+++ app.js
@@ -42,8 +42,11 @@ app.use(function(err, req, res, next) {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+// app.listen(port, () => {
+//   console.log(`Server is running at http://localhost:${port}`);
+// });
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on port ${port}`);
 });
 
 module.exports = app;
